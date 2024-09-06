0a1cd7675c94cb2fb61f7c08c0342589dd7abd55
RamHiren <118017707+RamHiren@users.noreply.github.com>
2024-09-06 23:32:27 +0530

    Time: 2 ms (14.89%), Space: 42.2 MB (5.27%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] a) {
+        // int[] b = new int[a.length];
+        for(int i=0;i<a.length-1;i++){
+            int min = i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]<a[min]){
+                    min = j;
+                }
+            }
+           int t = a[i];
+           a[i]=a[min];
+           a[min]=t;
+        }
+        
+    }
+}
