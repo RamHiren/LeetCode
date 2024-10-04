1b04b7a1276332adc82fd3a4f52035683b591c6d
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2024-10-04 11:49:23 +0530

    Time: 0 ms (100%), Space: 56.4 MB (33.15%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,17 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] a) {
-        int s =0;
-        int e = a.length-1;
-
-        while(s<e){
-            int m = s +(e-s)/2;
-
-            if(a[m]>a[m+1]){
-                e=m;
+        int s = 1;
+        int e = a.length-2;
+        
+        while(s<=e){
+            int m = (s+e)/2;
+            if(a[m]>a[m-1] && a[m+1]<a[m]){
+              return m;
+            }
+            else if(a[m+1]>a[m]){
+                s = m+1;
             }else{
-                s=m+1;
+                e=m-1;
             }
         }
-        return s;
+        return 0;
     }
 }
