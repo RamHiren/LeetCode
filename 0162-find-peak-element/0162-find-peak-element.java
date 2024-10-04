936e8670a9657b7af96bc060186af0057ca345f2
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2024-10-04 12:41:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (58.07%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] a) {
+        if(a.length == 1){
+            return 0;
+        }
+        if(a.length == 2){
+            if(a[0]>a[1]) return 0;
+            else return 1;
+        }
+        int s =1;
+        int e = a.length -1;
+
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+            
+            if(a[m]>a[(m+1)%a.length]&&a[m]>a[m-1]){
+                return m;
+            }else if(a[m]<a[(m+1)%a.length]){
+                s = m +1;
+            }else{
+                e = m-1;
+            }
+        }
+        return 0;
+    }
+}
