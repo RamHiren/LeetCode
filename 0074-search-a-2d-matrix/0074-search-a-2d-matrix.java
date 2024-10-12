4b47260f132ca01604d414d450b8188d2f8e4288
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2024-10-12 16:26:19 +0530

    Time: 0 ms (100%), Space: 42.1 MB (38.79%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] a, int t) {
+        int r=0;
+        int c = a[0].length - 1;
+
+        while(r<a.length && c> -1){
+            if(a[r][c]==t){
+                return true;
+            }
+            else if(a[r][c] < t){
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return false; 
+    }
+}
