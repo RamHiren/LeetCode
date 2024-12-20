917e17177aae3c1272d3a2be198e7306010af247
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2024-12-20 15:06:08 +0530

    Time: 5 ms (93.79%), Space: 55.7 MB (9.72%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] a) {
+        List<Integer> l = new ArrayList<>();
+
+        int i =0;
+        while(i<a.length){
+            int correct_i = a[i]-1;
+            if(a[i] != a[correct_i]){
+                int t = a[i];
+                a[i] = a[correct_i];
+                a[correct_i]=t;
+            }else{
+                i++;
+            }
+        }
+
+        for(int j=0; j<a.length;j++){
+            if(a[j] != j+1)
+                l.add(a[j]);
+        }
+
+        return l;
+    }
+}
