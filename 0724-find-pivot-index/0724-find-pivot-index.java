aecbc5f2db51e7eec224d65583efa063775a3c77
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-02-01 23:58:30 +0530

    Time: 383 ms (5.04%), Space: 45.7 MB (45.26%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] a) {
+        for(int i=0;i<a.length;i++){
+            int l=0,r=0;
+            for(int j=0;j<i;j++){
+                l +=a[j];
+            }
+            for(int j=i+1;j<a.length;j++){
+                r +=a[j];
+            }
+
+            if(r == l) return i;
+        }
+        return -1;
+    }
+}
