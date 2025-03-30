da6fb6aebf28d5b5cdedea6081e21cac0b520def
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-03-30 11:08:46 +0530

    Time: 17 ms (51.21%), Space: 54.2 MB (81.7%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,21 +1,39 @@
 class Solution {
     public int minSwaps(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
+
+        // for(int i =0;i<s.length();i++){
+        //     char c = s.charAt(i);
+
+        //     if(c ==  '['){
+        //         st.push(c);
+        //     }else{
+        //         if(st.isEmpty()||st.peek() == ']'){
+        //             st.push(c);
+        //         }else{
+        //             st.pop();
+        //         }
+        //     }
+        // }
+        // int open = st.size()/2;
+        // return (open+1)/2;
+
+
+        int open=0,close=0;
 
         for(int i =0;i<s.length();i++){
             char c = s.charAt(i);
-
-            if(c ==  '['){
-                st.push(c);
+            if(c== '['){
+                open++;
             }else{
-                if(st.isEmpty()||st.peek() == ']'){
-                    st.push(c);
+                if(open <= 0){
+                    close++;
                 }else{
-                    st.pop();
+                    open--;
                 }
             }
         }
-        int open = st.size()/2;
-        return (open+1)/2;
+
+        return (open + 1)/2;
     }
 }
