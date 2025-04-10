827677f34963ceea2bc142d9517c4c4f06525ee2
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-04-10 13:47:58 +0530

    Time: 2 ms (29.07%), Space: 57.4 MB (51.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,16 @@
 class Solution {
     public void rotate(int[] a, int k) {
-        int[] b = new int[a.length];
-        int l = a.length;
+        int t[] = new int[a.length];
         int j=0;
-        
-        
 
-        for (int i = k; i < b.length+k; i++) {
-            b[i%a.length]=a[j];
-            j++;
-        }     
+
+        for(int i =k;i<a.length+k;i++){
+            t[i%a.length] = a[j++];
+        }
 
         for(int i=0;i<a.length;i++){
-            a[i]=b[i];
+            a[i] = t[i];
         }
-        
+
     }
 }
