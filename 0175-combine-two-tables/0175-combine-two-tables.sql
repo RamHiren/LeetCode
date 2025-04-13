e3fb27c39dd70fb7f395022ff069c03ab4fe67f9
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-04-13 14:55:39 +0530

    Time: 549 ms (65.13%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,7 +1,10 @@
-SELECT
-  Person.firstName,
-  Person.lastName,
-  Address.city,
-  Address.state
-FROM Person
-LEFT JOIN Address ON Person.personId = Address.personId;
+-- SELECT
+--   Person.firstName,
+--   Person.lastName,
+--   Address.city,
+--   Address.state
+-- FROM Person
+-- LEFT JOIN Address ON Person.personId = Address.personId;
+
+select Person.firstName,Person.lastName,Address.city,Address.state From Person
+left join Address on Person.personId = Address.personId;
