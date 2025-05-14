f5e93e24a240830155874f75d38fc7973ffbb722
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-05-14 13:30:28 +0530

    Time: 2122 ms (13.38%), Space: 40.6 MB (31.53%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        int sum =0;
+
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                System.out.println(i);
+                sum += i;
+            }
+        }
+
+        return sum == n;
+    }
+}
