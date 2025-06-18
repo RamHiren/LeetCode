4875ef1a9583eeb852731df2eb20764ac0f6851d
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-18 11:00:59 +0530

    Time: 2 ms (74.93%), Space: 61.8 MB (41.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,12 +3,11 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int ans = 0;
 
-
         for(int i=0;i<a.length;i++){
             if(min>a[i]){
                 min = a[i];
             }else{
-                ans = Math.max(ans, a[i]-min);
+                ans = Math.max(ans,a[i]-min);
             }
         }
         return ans;
