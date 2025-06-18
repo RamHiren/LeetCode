7691f10acd70276e9cd19803495eef6e87ef8ab9
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-18 15:47:38 +0530

    Time: 0 ms (100%), Space: 45.8 MB (83.92%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,50 @@
 class Solution {
     public int[] searchRange(int[] a, int t) {
-        int [] ans= {-1,-1};
-        int f =0;
-        
-        for(int i=0;i<a.length;i++){
-            if(a[i]==t){
-                if(f==0){
-                    ans[0] = i;
-                    f=1;
-                }
-                ans[1]=i;
+        int l = left(a,t);
+        int r = right(a,t);
+
+        return new int[]{l,r};
+
+    }
+
+    public int left(int[]a,int t){
+        int ans = -1;
+        int s = 0;
+        int e = a.length-1;
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+
+            if(a[m]==t){
+                ans = m;
+                e = m-1;
+            }
+            else if(a[m]<t){
+                s = m+1;
+            }else{
+                e = m-1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int right(int[]a,int t){
+        int ans = -1;
+        int s = 0;
+        int e = a.length-1;
+
+        while(s<=e){
+            int m = s + (e-s)/2;
+
+            if(a[m]==t){
+                ans = m;
+                s = m+1;
+            }
+            else if(a[m]<t){
+                s = m+1;
+            }else{
+                e = m-1;
             }
         }
 
