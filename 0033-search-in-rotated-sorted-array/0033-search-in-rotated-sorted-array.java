f88b247c67ca5a7b4cfb54edb64dd40eb52d5db6
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-18 14:36:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,28 @@
 class Solution {
     public int search(int[] a, int t) {
         int s =0;
-        int e = a.length-1;
+        int e =a.length-1;
 
         while(s<=e){
-            int m = s+(e-s)/2;
-            if(a[m]==t){
-                return m;
-            }
-            if(a[s]<=a[m]){ // left part sorted
-                if(a[s]<=t && t<=a[m]){ //Left ma element aavelo che
+            int m = s + (e-s)/2;
+
+            if(a[m]==t) return m;
+
+            if(a[s]<=a[m]){ 
+                if(a[s]<=t && t<a[m]){
                     e = m-1;
-                }else{ //right ma element aavelo che
+                }else{
                     s = m+1;
                 }
-            }else{ //Right Part Sorted
-                if(a[m]<=t && t<=a[e]){ //left ma j elemant aavelo che 
+            }else{
+                if(a[m]<=t && t<=a[e]){
                     s = m+1;
-                }else{ //right ma j elemant aavelo che
-                    e = m-1;
+                }else{
+                    e=m-1;
                 }
             }
         }
+
         return -1;
     }
 }
