6fe3bd8ece6af738f934637432b096b1105a3aa7
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-19 21:17:19 +0530

    Time: 7 ms (42.46%), Space: 45.5 MB (53.52%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,42 +2,41 @@ class Solution {
     public int[] asteroidCollision(int[] a) {
         Stack<Integer> s = new Stack<>();
 
-
         for(int i=0;i<a.length;i++){
-            if(s.isEmpty() || a[i] >0){
+            if(s.isEmpty() || s.peek()<0){
                 s.push(a[i]);
             }else{
-                while(!s.isEmpty()){
-                    int t = s.peek();
-                    if(t<0){
-                        s.push(a[i]);
-                        break;
-                    }
-                    int abVal = Math.abs(a[i]);
-
-                    if(t<abVal){
-                        s.pop();
-                        if(s.isEmpty()){
-                            s.push(a[i]);
+                if(s.peek()>0 && a[i]>0){
+                    s.push(a[i]);
+                }else{
+                    while(!s.isEmpty()){
+                        if(s.peek()==Math.abs(a[i])){
+                            s.pop();
                             break;
                         }
-                    }else if(t == abVal){
-                        s.pop();
-                        break;
-                    }else{
-                        break;
+                        if(s.peek()>Math.abs(a[i])||s.peek()<0){
+                            break;
+                        }else{
+                            s.pop();
+                            if(s.isEmpty() || s.peek()<0)s.push(a[i]);
+                        }
                     }
                 }
             }
         }
 
-        int l = s.size();
-        int [] ans = new int[l];
+        ArrayList<Integer> l =new ArrayList<>();
+        while(!s.isEmpty()){
+            l.add(s.pop());
+        }
+        Collections.reverse(l);
 
-        for(int i=l-1; i>=0;i--){
-            ans[i] = s.pop();
+        int b[] = new int[l.size()];
+
+        for(int i=0;i<b.length;i++){
+            b[i] = l.get(i);
         }
 
-       return ans;
+        return b;
     }
 }
