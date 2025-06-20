a9610e6019392e2026709bd428b589f0d368e4cc
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-20 18:04:54 +0530

    Time: 25 ms (41.24%), Space: 45 MB (75.6%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,24 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] a, int h) {
-         Arrays.sort(a);
-        int s = 1, e = a[a.length - 1];
-        // System.out.println(e);
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (check(m, a) <= h) {
-                e = m - 1;
-            } else {
-                s = m + 1;
+        
+        int s =1;
+        int e = Integer.MIN_VALUE;
+        for(int i:a) e = Math.max(e,i);
+
+        while(s<e){
+            int m = s + (e-s)/2;
+
+            if(check(a,m,h)){
+                e = m;
+            }else{
+                s = m+1;
             }
+
         }
-        return s;
+
+            return s;
+
     }
 
-    static long check(int m, int[] a) {
-        long total = 0;
-        for (int i = 0; i < a.length; i++) {
-            total = total + (long)Math.ceil((double)a[i]/m);
+    boolean check(int[] a,int m,int h){
+        int t =0;
+        for(int i=0;i<a.length;i++){
+            t += Math.ceil((double)(a[i])/(double)(m));
         }
-        return total;
+        return t<=h;
     }
 }
