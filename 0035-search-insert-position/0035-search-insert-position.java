dfa4134f47ea617dd76b9220e589be90270515cf
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-06-26 15:51:51 +0530

    Time: 0 ms (100%), Space: 43.1 MB (39.39%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,16 +1,19 @@
 class Solution {
     public int searchInsert(int[] a, int t) {
-        int s =0;
-        int e = a.length -1;
+        int s=0;
+        int e =a.length-1;
+
         while(s<=e){
             int m = s + (e-s)/2;
+
             if(a[m]==t){
                 return m;
-            }else if(t>a[m]){
-                s  = m+1;
+            }else if(a[m]>t){
+                e = m-1;
             }else{
-                e=m-1;
+                s =m+1;
             }
+
         }
         return s;
     }
