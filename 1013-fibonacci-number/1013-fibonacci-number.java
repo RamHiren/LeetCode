e5f28e4d0a8536edf9f8723ffc238005ece0ce21
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-14 22:53:28 +0530

    Time: 0 ms (100%), Space: 40.4 MB (64.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
+        if(n<=1) return n;
+        int f =0;
+        int s=1;
+        int ans =0;
+        for(int i=2;i<=n;i++){
+            ans = f+s;
+            f = s;
+            s =ans;
         }
-        return fib(n-1)+fib(n-2);
+        return ans;
         
     }
 }
