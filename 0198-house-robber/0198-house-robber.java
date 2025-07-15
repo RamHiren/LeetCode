9d3efa7bc17b8869c2497cfee330f5cfd7a5cc22
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-15 13:20:16 +0530

    Time: 0 ms (100%), Space: 41.3 MB (24.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -22,27 +22,41 @@ class Solution {
     // }
 
 
+    // public int rob(int[] a) {
+    //     int dp[] = new int[a.length+1];
+    //     Arrays.fill(dp,-1);
+    //     return rec(a,a.length,dp);
+    // }
+
+    // public int rec(int[] a,int i,int[] dp){
+    //     if(i==1){
+    //         dp[i] = a[0];
+    //         return a[0];
+    //     }
+    //     if(i==0){
+    //         dp[i] = 0;
+    //         return 0;
+    //     }
+
+    //     if(dp[i]!=-1)return dp[i];
+
+    //     int pick = a[i-1]+rec(a,i-2,dp);
+    //     int nopick = 0+ rec(a,i-1,dp);
+    //     dp[i] = Math.max(pick,nopick);
+    //     return dp[i];
+    // }
+
     public int rob(int[] a) {
         int dp[] = new int[a.length+1];
-        Arrays.fill(dp,-1);
-        return rec(a,a.length,dp);
+        dp[0] = 0;
+        dp[1] = a[0];
+       for(int i=2;i<a.length+1;i++){
+        int pick = a[i-1] + dp[i-2];
+        int noPick = 0 + dp[i-1];
+        dp[i] = Math.max(pick,noPick);
+       }
+        return dp[a.length];
     }
 
-    public int rec(int[] a,int i,int[] dp){
-        if(i==1){
-            dp[i] = a[0];
-            return a[0];
-        }
-        if(i==0){
-            dp[i] = 0;
-            return 0;
-        }
-
-        if(dp[i]!=-1)return dp[i];
-
-        int pick = a[i-1]+rec(a,i-2,dp);
-        int nopick = 0+ rec(a,i-1,dp);
-        dp[i] = Math.max(pick,nopick);
-        return dp[i];
-    }
+    
 }
