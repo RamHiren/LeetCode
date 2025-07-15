61ddad80e0baf6229469c795238ef1080a958060
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-15 13:15:13 +0530

    Time: 0 ms (100%), Space: 41.1 MB (60.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,48 @@
 class Solution {
+    // public int rob(int[] a) {
+    //     int dp[] = new int[a.length];
+    //     Array.fill(dp,-1);
+    //     return rec(a,a.length-1);
+    // }
+
+    // public int rec(int[] a,int i,dp){
+    //     if(i==0){
+    //         dp[i] = a[0];
+    //         return a[0];
+    //     }
+    //     if(i==-1){
+    //         dp[i] = -
+    //         return 0;
+    //     }
+
+    //     if(dp!=-1)return dp[i];
+
+    //     int last = Math.max(a[i]+rec(a,i-2),0+rec(a,i-1));
+    //     return last;
+    // }
+
+
     public int rob(int[] a) {
-       if (a.length < 2) {
+        int dp[] = new int[a.length+1];
+        Arrays.fill(dp,-1);
+        return rec(a,a.length,dp);
+    }
+
+    public int rec(int[] a,int i,int[] dp){
+        if(i==1){
+            dp[i] = a[0];
             return a[0];
         }
+        if(i==0){
+            dp[i] = 0;
+            return 0;
+        }
 
-       int[] b = new int[a.length];
+        if(dp[i]!=-1)return dp[i];
 
-       b[0] = a[0];
-       b[1] = Math.max(a[0], a[1]);
-
-       for (int i = 2; i < b.length; i++) {
-            b[i] = Math.max(b[i-2]+a[i], b[i-1]);
-       }
-
-       return b[b.length - 1]; 
+        int pick = a[i-1]+rec(a,i-2,dp);
+        int nopick = 0+ rec(a,i-1,dp);
+        dp[i] = Math.max(pick,nopick);
+        return dp[i];
     }
 }
