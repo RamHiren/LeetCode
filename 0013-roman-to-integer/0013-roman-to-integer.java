40b9073867dc8e2bfa9ccef0aad0c1c2004d2ef2
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-17 16:10:35 +0530

    Time: 12 ms (5.16%), Space: 45.6 MB (5.01%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,72 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> m = new HashMap<>();
+        HashMap<String,Integer> m = new HashMap<>();
+        int ans =0;
+        m.put("I",1);
+        m.put("IV",4);
+        m.put("V",5);
+        m.put("IX",9);
+        m.put("X",10);
+        m.put("XL",40);
+        m.put("L",50);
+        m.put("XC",90);
+        m.put("C",100);
+        m.put("CD",400);
+        m.put("D",500);
+        m.put("CM",900);
+        m.put("M",1000);
 
-        m.put('I',1);
-        m.put('V',5);
-        m.put('X',10);
-        m.put('L',50);
-        m.put('C',100);
-        m.put('D',500);
-        m.put('M',1000);
-
-        int a =0;
-
-        for(int i =0;i<s.length();i++){
-            if(i < s.length()-1 && m.get(s.charAt(i))<m.get(s.charAt(i+1))){
-                a = a + ( m.get(s.charAt(i+1)) - m.get(s.charAt(i)));
-                i++;
+        for(int i=0;i<s.length();i++){
+            if(i==s.length()-1){
+                ans += m.get(s.charAt(i)+"");
             }else{
-                a += m.get(s.charAt(i));
+
+                String t= ""+s.charAt(i)+s.charAt(i+1);
+                if(m.containsKey(t)){
+                    ans = ans + m.get(t);
+                    i++;
+                }else{
+                    String t1 = ""+ s.charAt(i);
+                    // System.out.println(t1);
+                    // System.out.println(m.get("I"));
+
+                    ans = ans+ m.get(t1);
+                }
             }
         }
 
 
 
-        return a;
+        return ans;
     }
+
+    // public int romanToInt(String s) {
+    //    String str [] = {"I","IV","V","IX","X","L","XC","C","D","CM","M"};
+    //    int a [] = {1,4,5,9,10,50,90,100,500,900,1000};
+    //    int ans=0;
+       
+
+    //    for(int i=0;i<s.length();i++){
+    //         if(i==s.length()-1){
+    //             ans += ;
+    //         }else{
+
+    //             String t= ""+s.charAt(i)+s.charAt(i+1);
+    //             if(m.containsKey(t)){
+    //                 ans = ans + m.get(t);
+    //                 i++;
+    //             }else{
+    //                 String t1 = ""+ s.charAt(i);
+    //                 System.out.println(t1);
+    //                 System.out.println(m.get("I"));
+
+    //                 ans = ans+ m.get(t1);
+    //             }
+    //         }
+    //     }
+
+
+
+    //     return ans;
+    // }
 }
