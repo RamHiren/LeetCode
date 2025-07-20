0b1f4317ad65cd960b3ce51d616112ea7d9d4282
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-20 12:55:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (5.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -8,16 +8,16 @@ class Solution {
     }
 
     public void helper(List<List<Integer>> l,int []a, List<Integer> t,int i){
-        l.add(new ArrayList<>(t));
-
-        for(int j=i;j<a.length;j++){
-            t.add(a[j]);
-
-            helper(l,a,t,j+1);
-
-            t.remove(t.size()-1);
-
+        if(i==a.length){
+            l.add(new ArrayList<>(t));
+            return;
         }
+        //pick
+        t.add(a[i]);
+        helper(l,a,t,i+1);
+        // no pick
+        t.remove(t.size()-1);
+        helper(l,a,t,i+1);
     }
 
 
