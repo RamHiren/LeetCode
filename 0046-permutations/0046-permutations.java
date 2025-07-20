4c81831b60654fa940f758bc0bedb2eea7bda663
Ram Hiren <118017707+RamHiren@users.noreply.github.com>
2025-07-20 23:36:11 +0530

    Time: 2 ms (36.86%), Space: 44.9 MB (18.28%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,34 @@
-class Solution {
-    public List<List<Integer>> permute(int[] a) {
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> l = new ArrayList<>();
-        List<Integer> l1 = new ArrayList<>();
-        helper(l,l1,a);
+        l.add(new ArrayList<>());
 
-        return l;
-    }
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
 
-    public void helper(List<List<Integer>> l,List<Integer> l1,int[] a){
-        if(l1.size()==a.length){
-            l.add(new ArrayList<>(l1));
-            return;
-        }
-
-        for(int n:a){
-            if(l1.contains(n)){
+            if (l.size() == 1 && l.get(0).isEmpty()) {
+                List<Integer> first = new ArrayList<>();
+                first.add(num);
+                l.set(0, first); // replace the empty list
                 continue;
             }
 
-            l1.add(n);
-            helper(l,l1,a);
-            l1.remove(l1.size()-1);
+            List<List<Integer>> m = l;
+            List<List<Integer>> n = new ArrayList<>();
+
+            for (List<Integer> j : m) {
+                for (int k = 0; k <= j.size(); k++) {
+                    List<Integer> temp = new ArrayList<>(j);
+                    temp.add(k, num);
+                    n.add(temp);
+                }
+            }
+
+            l = n;
         }
+
+        return l;
     }
 }
